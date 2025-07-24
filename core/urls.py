de29fd3c76ac6a8c8eed7a68e1db6be4bf26d3cf
Hatef Barin <amir.barin76@gmail.com>
2025-07-24 13:31:16 +0430

    Set up URLs for UserViewSet

---
+++ core/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+
+from core.views import UserViewSet
+
+
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='users')
+
+
+
+urlpatterns = router.urls
