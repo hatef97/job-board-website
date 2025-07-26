14616be37e3f4877a306de3f2e2338fbce331b4e
Hatef Barin <amir.barin76@gmail.com>
2025-07-26 20:51:10 +0430

    Add Set up URLs for EmployerProfileViewSet

--- core/urls.py
+++ core/urls.py
@@ -2,12 +2,13 @@ from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 
-from core.views import UserViewSet, CheckEmailView
+from core.views import UserViewSet, CheckEmailView, EmployerProfileViewSet
 
 
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='users')
+router.register(r'employer-profiles', EmployerProfileViewSet, basename='employer-profiles')
 
 
 
