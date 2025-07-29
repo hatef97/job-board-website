39a3b437fb12b1ab65fb240b1c5e9162031dd9c1
Hatef Barin <amir.barin76@gmail.com>
2025-07-29 20:40:46 +0430

    Add CategoryAdmin

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Tag, CompanyProfile, Job
+
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+    ordering = ('name',)
+    
