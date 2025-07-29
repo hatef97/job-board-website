4516e6bbd5b0dca6270ca11e80973d742769e74d
Hatef Barin <amir.barin76@gmail.com>
2025-07-29 20:43:35 +0430

    Add CompanyProfileAdmin

--- jobs/admin.py
+++ jobs/admin.py
@@ -18,4 +18,21 @@ class TagAdmin(admin.ModelAdmin):
     list_display = ('name',)
     search_fields = ('name',)
     ordering = ('name',)
+
+
+
+@admin.register(CompanyProfile)
+class CompanyProfileAdmin(admin.ModelAdmin):
+    list_display = ('company_name', 'user', 'location')
+    search_fields = ('company_name', 'user__email', 'location')
+    autocomplete_fields = ('user',)
+    list_filter = ('location',)
+    readonly_fields = ('logo_preview',)
+
+    def logo_preview(self, obj):
+        if obj.logo:
+            return f'<img src="{obj.logo.url}" width="80" height="80" style="object-fit:cover;" />'
+        return "-"
+    logo_preview.allow_tags = True
+    logo_preview.short_description = "Logo Preview"
     
