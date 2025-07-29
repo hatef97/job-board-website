e44e3c5ef26545cb945847baf8b564bfd2d87f5c
Hatef Barin <amir.barin76@gmail.com>
2025-07-29 13:13:56 +0430

    Add CategoryModel

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.conf import settings
+from django.utils.text import slugify
 
-# Create your models here.
+
+
+# ==========================
+# CHOICES
+# ==========================
+
+JOB_TYPE_CHOICES = [
+    ('full_time', 'Full Time'),
+    ('part_time', 'Part Time'),
+    ('contract', 'Contract'),
+    ('remote', 'Remote'),
+    ('internship', 'Internship'),
+]
+
+EXPERIENCE_CHOICES = [
+    ('junior', 'Junior'),
+    ('mid', 'Mid-level'),
+    ('senior', 'Senior'),
+    ('lead', 'Lead'),
+]
+
+
+
+# ==========================
+# CATEGORY MODEL
+# ==========================
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+        
