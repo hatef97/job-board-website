3b69e30abfc4ea7783b39045e5a57e4347f4846c
Hatef Barin <amir.barin76@gmail.com>
2025-07-30 00:47:14 +0430

    Add CompanyProfileSerializer

--- jobs/serializers.py
+++ jobs/serializers.py
@@ -25,3 +25,26 @@ class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = ['id', 'name']
+
+
+
+# ==========================
+# COMPANY PROFILE
+# ==========================
+
+class CompanyProfileSerializer(serializers.ModelSerializer):
+    user_email = serializers.EmailField(source='user.email', read_only=True)
+    logo_url = serializers.SerializerMethodField()
+
+    class Meta:
+        model = CompanyProfile
+        fields = [
+            'id', 'user', 'user_email', 'company_name', 'website',
+            'logo', 'logo_url', 'location', 'description'
+        ]
+
+    def get_logo_url(self, obj):
+        request = self.context.get('request')
+        if obj.logo and request:
+            return request.build_absolute_uri(obj.logo.url)
+        return None
