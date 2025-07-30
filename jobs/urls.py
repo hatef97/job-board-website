076869566a941c8ae87858f4e0952982eab63a16
Hatef Barin <amir.barin76@gmail.com>
2025-07-30 18:22:46 +0430

    Add Set up URLs for CategoryViewSet

---
+++ jobs/urls.py
@@ -0,0 +1,12 @@
+from rest_framework.routers import DefaultRouter
+
+from .views import *
+
+
+
+router = DefaultRouter()
+router.register('categories', CategoryViewSet)
+
+
+
+urlpatterns = router.urls
