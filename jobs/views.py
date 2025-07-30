9d7e2441c4b85e5d7214635419bf209771a76116
Hatef Barin <amir.barin76@gmail.com>
2025-07-30 18:15:59 +0430

    Add CategoryViewSet

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from rest_framework import viewsets, permissions, mixins
+from rest_framework.response import Response
+from rest_framework.exceptions import PermissionDenied
+
+from .models import *
+from .serializers import *
+
+from core.models import User
+from django.shortcuts import get_object_or_404
+
+
+
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.AllowAny]
